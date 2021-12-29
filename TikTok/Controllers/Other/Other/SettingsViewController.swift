874ef3ba79c023b8496f8f6a175fb19cbb3fe59a
mai nguyen <maing528@hotmail.com>
2021-12-28 17:21:57 -0800

    Terms & Privacy Policy

--- TikTok/Controllers/Other/Other/SettingsViewController.swift
+++ TikTok/Controllers/Other/Other/SettingsViewController.swift
@@ -5,18 +5,60 @@
 //  Created by mai ng on 7/25/21.
 //
 
+import SafariServices
 import UIKit
 
+
+struct SettingsSection {
+    let title:String
+    let options: [SettingsOption]
+}
+
+struct SettingsOption {
+    let title: String
+    let handler:(() -> Void)
+}
 class SettingsViewController: UIViewController {
     private let tableView: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return table
     }()
-
     
+    var sections = [SettingsSection]()
+
+//    DispatchQueue.main.async {
+//        guard let url = URL(string: "https://www.tiktok.com/legal/terms-of-service")
+//     else {
+//        return
+//    }
+//    let vc = SFSafariViewController(url: url)
+//    self?.present(vc, animated: true, completion: true)
     override func viewDidLoad() {
         super.viewDidLoad()
+        sections = [SettingsSection(title: "Information",
+                                    options: [
+                                        SettingsOption(title: "Terms of Service", handler: { [weak self] in
+                                            DispatchQueue.main.async {
+                                                guard let url = URL(string: "https://www.tiktok.com/legal/terms-of-service?lang=en") else {
+                                                    return
+                                                }
+                                                let vc = SFSafariViewController(url: url)
+                                                self?.present(vc, animated: true)
+                                            }
+                                        }),
+                                        SettingsOption(title: "Privacy Policy", handler: { [weak self] in
+                                            DispatchQueue.main.async {
+                                                guard let url = URL(string: "https://www.tiktok.com/legal/privacy-policy") else {
+                                                    return
+                                                }
+                                                let vc = SFSafariViewController(url: url)
+                                                self?.present(vc, animated: true)
+                                            }
+                                        })])]
+                          
+                        
+
         title = "Settings"
         view.backgroundColor = .systemBackground
         view.addSubview(tableView)
@@ -79,15 +121,27 @@ class SettingsViewController: UIViewController {
 
 
 extension SettingsViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return sections[section].options.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = sections[indexPath.section].options[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Hello World"
+        
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.text = model.title
         return cell
     }
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let model = sections[indexPath.section].options[indexPath.row]
+        model.handler()
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].title
+    }
     
 }
